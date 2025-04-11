43dd2ec7cfcaa472801998783e93bc3d0e4fad19
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-04-11 00:55:50 -0300

    Feature: Implementing firebase repository

--- lib/interface/app.module.dart
+++ lib/interface/app.module.dart
@@ -1,6 +1,9 @@
 import 'package:play_list/repository/jogo.repository.dart';
+import 'package:play_list/repository/jogo_firebase.repository.dart';
 import 'package:play_list/repository/jogo_memo.repository.dart';
 
 class AppModule {
-  static JogoRepository get jogoRepository => JogoMemoryRepository.instance;
+  // static JogoRepository get jogoRepository => JogoMemoryRepository.instance;
+  static JogoRepository get jogoRepository => JogoFirebaseRepository.instance;
+
 }
--- lib/repository/jogo_firebase.repository.dart
+++ lib/repository/jogo_firebase.repository.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:play_list/model/jogo.dart';
 import 'package:play_list/repository/jogo.repository.dart';
 
@@ -10,14 +11,30 @@ class JogoFirebaseRepository implements JogoRepository {
 
   @override
   Future<void> addJogo(Jogo jogo) async {
-    // TODO: implement addJogo
-    throw UnimplementedError();
+    try {
+      await FirebaseFirestore.instance.collection("games").add(jogo.toMap());
+    } catch (e) {
+      throw ErroConexao();
+    }
   }
 
   @override
   Future<List<Jogo>> getJogos() async {
-    // TODO: implement getJogos
-    throw UnimplementedError();
-  }
+    try {
+      final snapshot = await FirebaseFirestore.instance.collection('games').get();
 
+      List<Jogo> games = [];
+      for (var doc in snapshot.docs) {
+        games.add(Jogo.deMapa(doc.data()));
+      }
+      return games;
+    } catch (e) {
+      throw ErroConexao();
+    }
+  }
+}
+
+class ErroConexao implements Exception {
+  @override
+  String toString() => "Erro ao conectar!!";
 }
